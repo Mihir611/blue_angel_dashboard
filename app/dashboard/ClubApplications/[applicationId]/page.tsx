a4e3b3d5b0bfb185666ac01853a0fbe0aad5af35
Mihir611 <mihir17.udupa@gmail.com>
2026-08-21 13:51:45 +0530

    Fixed the applications issue

---
+++ app/dashboard/ClubApplications/[applicationId]/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "@/app/components/dashboard/DashboardLayout";
+
+const ApproveApplications = () => {
+    return (
+        <DashboardLayout title="Approve Applications">
+            <div>
+                <p>Coming Soon !!</p>
+            </div>
+        </DashboardLayout>
+    )
+}
+
+export default ApproveApplications
